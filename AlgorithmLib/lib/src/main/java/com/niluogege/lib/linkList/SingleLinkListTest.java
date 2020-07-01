f49f906182ceb309bf2e22230054f67045ecb4c2
luoChen <hank.luo@xianghuanji.com>
2020-07-01 13:23:02 +0800

    链表

--- AlgorithmLib/lib/src/main/java/com/niluogege/lib/linkList/SingleLinkListTest.java
+++ AlgorithmLib/lib/src/main/java/com/niluogege/lib/linkList/SingleLinkListTest.java
@@ -8,11 +8,28 @@ public class SingleLinkListTest {
     public static void main(String[] args) {
 
 
-        Node node = craeteLinkList();
-        iterateLinkList(node);
-        Node reverseNode = reverseByloop(node);
+        //测试翻转
+//        Node node = craeteLinkList(1);
+//        iterateLinkList(node);
+//        Node reverseNode = reverseByloop(node);
+//        System.out.println("");
+//        iterateLinkList(reverseNode);
+
+        //测试merge
+        // one=02468
+        Node one = craeteLinkList(2);
+        iterateLinkList(one);
+
         System.out.println("");
-        iterateLinkList(reverseNode);
+
+        //two=0369
+        Node two = craeteLinkList(3);
+        iterateLinkList(two);
+
+        System.out.println("");
+
+        Node merged = mergeListByRe(one, two);
+        iterateLinkList(merged);
     }
 
 
@@ -21,11 +38,11 @@ public class SingleLinkListTest {
      *
      * @return
      */
-    private static Node craeteLinkList() {
+    private static Node craeteLinkList(int diff) {
 
         Node header = null;
         Node pre = null;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i += diff) {
             Node node = new Node(i);
             if (header == null) {
                 header = node;
@@ -57,6 +74,7 @@ public class SingleLinkListTest {
 
     /**
      * 链表翻转 - 遍历的方式
+     *
      * @return
      */
     private static Node reverseByloop(Node header) {
@@ -72,7 +90,7 @@ public class SingleLinkListTest {
 
         while (header != null) {
             // 记录下一个节点
-            nextNode=header.next;
+            nextNode = header.next;
 
             //设置当前节点 的下一个结节 为前一个节点
             header.next = preNode;
@@ -82,11 +100,31 @@ public class SingleLinkListTest {
 
 
             //切换到下一个节点
-            header=nextNode;
+            header = nextNode;
 
         }
 
         return preNode;
+    }
+
+
+    /**
+     * 合并两个有序链表 - 递归
+     */
+    private static Node mergeListByRe(Node one, Node two) {
+        if (one == null) return two;
+        if (two == null) return one;
+
+        Node head = null;
+
+        if (one.value <= two.value) {
+            head = one;
+            head.next = mergeListByRe(one.next, two);
+        } else {
+            head = two;
+            head.next = mergeListByRe(one, two.next);
+        }
+        return head;
     }
 
 
