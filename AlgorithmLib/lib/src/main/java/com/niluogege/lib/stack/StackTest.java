6f656cec9118e605af764e320c8b32e183155166
luoChen <hank.luo@xianghuanji.com>
2020-09-02 10:13:55 +0800

    stackSequences

--- AlgorithmLib/lib/src/main/java/com/niluogege/lib/stack/StackTest.java
+++ AlgorithmLib/lib/src/main/java/com/niluogege/lib/stack/StackTest.java
@@ -5,15 +5,43 @@ import java.util.Stack;
 public class StackTest {
     public static void main(String[] args) {
 
-        MinStack minStack = new MinStack();
-        minStack.push(5);
-        minStack.push(2);
-        minStack.push(10);
+//        MinStack minStack = new MinStack();
+//        minStack.push(5);
+//        minStack.push(2);
+//        minStack.push(10);
+//
+//        int top = minStack.top();
+//        int min = minStack.getMin();
+//        int pop = minStack.pop();
+//        System.out.println("top= " + top + " min= " + min + " pop= " + pop);
 
-        int top = minStack.top();
-        int min = minStack.getMin();
-        int pop = minStack.pop();
-        System.out.println("top= " + top + " min= " + min + " pop= " + pop);
+        //------验证栈序列-------
+
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {4, 5, 3, 1, 2};
+        boolean stackSequences = validateStackSequences(arr1, arr2);
+        System.out.println("stackSequences= "+stackSequences);
+    }
+
+
+    /**
+     * 题目：验证栈序列
+     * 参考：https://leetcode-cn.com/problems/validate-stack-sequences/solution/yan-zheng-zhan-xu-lie-by-leetcode/
+     * 思路：模拟 pop 的过程
+     */
+    private static boolean validateStackSequences(int[] pushed, int[] popped) {
+        int N = pushed.length;
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+        for (int x : pushed) {
+            stack.push(x);
+            while (!stack.isEmpty() && j < N && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return j == N;
     }
 
 
