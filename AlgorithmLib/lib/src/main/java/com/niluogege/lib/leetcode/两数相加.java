ad15b58b447250cc65b74229c4c970e3a1b8408f
luoChen <hank.luo@xianghuanji.com>
2023-03-03 18:01:44 +0800

    两数相加 通过啦

--- AlgorithmLib/lib/src/main/java/com/niluogege/lib/leetcode/两数相加.java
+++ AlgorithmLib/lib/src/main/java/com/niluogege/lib/leetcode/两数相加.java
@@ -4,6 +4,16 @@ package com.niluogege.lib.leetcode;
 import com.niluogege.lib.javabean.ListNode;
 import com.niluogege.lib.utils.LinklistUtils;
 
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class 两数相加 {
 
     public static void main(String[] args) {
@@ -23,6 +33,47 @@ public class 两数相加 {
     }
 
     private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        //创建虚拟头结点，方便操作
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+
+        //用于记录进位
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+
+            if (l2 != null)
+                l2 = l2.next;
+
+        }
+
+        //说明最后一次相加 有进位所以要补一位
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+
+        return pre.next;
+    }
+
+    /**
+     * 我的算法 和标准答案的算法差不多，不过我的有以下几个可以优化的点
+     * 1. 可以先不用计算两个 链表长度，可以 同时遍历两个链表，如果长度不够直接按0计算就行了
+     */
+    private static ListNode addTwoNumbers_me(ListNode l1, ListNode l2) {
         //获取链表最大长度
         int list1Length = getNodeLength(l1);
         int list2Length = getNodeLength(l2);
