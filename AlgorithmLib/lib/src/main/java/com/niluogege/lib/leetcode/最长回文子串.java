7cad33347f36164ff92923324f68fdf9c2737bc7
luoChen <hank.luo@xianghuanji.com>
2023-03-03 18:17:00 +0800

    。。

---
+++ AlgorithmLib/lib/src/main/java/com/niluogege/lib/leetcode/最长回文子串.java
@@ -0,0 +1,23 @@
+package com.niluogege.lib.leetcode;
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ *
+ * https://leetcode.cn/problems/longest-palindromic-substring/
+ */
+public class 最长回文子串 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * 具体解法：https://leetcode.cn/problems/longest-palindromic-substring/solution/zhong-xin-kuo-san-fa-he-dong-tai-gui-hua-by-reedfa/
+     */
+    public String longestPalindrome(String s) {
+
+    }
+
+}
