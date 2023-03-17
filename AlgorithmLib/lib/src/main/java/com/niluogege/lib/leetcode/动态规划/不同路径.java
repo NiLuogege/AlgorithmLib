55e15ddafd8fa736e20c540d83e2d7dd5c6a9c00
luoChen <hank.luo@xianghuanji.com>
2023-03-17 13:40:44 +0800

    动态规划

---
+++ AlgorithmLib/lib/src/main/java/com/niluogege/lib/leetcode/动态规划/不同路径.java
@@ -0,0 +1,53 @@
+package com.niluogege.lib.leetcode.动态规划;
+
+/**
+ * https://leetcode.cn/problems/unique-paths/
+ */
+public class 不同路径 {
+    public static void main(String[] args) {
+
+        System.out.println(uniquePaths(1, 1));
+//        System.out.println(uniquePaths(3, 3));
+//        System.out.println(uniquePaths(3, 7));
+    }
+
+    /**
+     * 动态规划五板斧
+     * 1. 确认dp数组及其下标含义
+     * 2. 确认递推公式
+     * 3. 确认dp数组初始值，及遍历遍历初始值
+     * 4. 确认遍历顺序
+     * 5. 有问题打印dp数组进行调试
+     */
+    public static int uniquePaths(int m, int n) {
+
+        //1. dp[i][j] 的含义为到达第 i,j 坐标有多少条路径
+        int[][] dp = new int[m][n];
+
+        //2. 递推公式 dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        //3. 确定dp初始值
+        //4. 顺序遍历
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.print(dp[i][j]);
+//            }
+//            System.out.println("");
+//        }
+//
+//        System.out.println("\n");
+
+        //
+        return dp[m - 1][n - 1];
+    }
+}
---
+++ AlgorithmLib/lib/src/main/java/com/niluogege/lib/leetcode/动态规划/爬楼梯_使用最小花费爬楼梯.java
@@ -0,0 +1,49 @@
+package com.niluogege.lib.leetcode.动态规划;
+
+/**
+ * https://leetcode.cn/problems/min-cost-climbing-stairs/
+ */
+public class 爬楼梯_使用最小花费爬楼梯 {
+
+    public static void main(String[] args) {
+//        int[] arr = new int[]{10, 15, 20};
+        int[] arr = new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+
+
+        System.out.println(minCostClimbingStairs(arr));
+    }
+
+    /**
+     * 动态规划五部曲
+     * 1. 确定dp数组和下标含义
+     * 2. 确定递推公式
+     * 3. 确定dp数组初始值和 遍历初始值
+     * 4. 确定遍历顺序
+     * 5. 有问题的时候打印dp数组进行调试
+     */
+    public static int minCostClimbingStairs(int[] cost) {
+
+        int n = cost.length;
+
+        //1. dp数组的含义是到第i个台阶花费的最低费用
+        int[] dp = new int[n];
+
+        //2. 递推公式  dp[i] = min(dp[i-1],dp[i-2])+ cost[i]  注意一定要加上当前台阶的值
+
+        //3. 确认初始值和遍历开始值
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        //4. 确认遍历顺序，后面依赖前面的就是 从小到到
+        for (int i = 2; i < n; i++) {
+            dp[i] += Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+
+        for (int i = 0; i < dp.length; i++) {
+            System.out.println(dp[i]);
+        }
+
+        System.out.println("\n");
+        return dp[n - 1];
+    }
+}
